59732e5ba05669c14fc3bcb119d2129849436ded
Jokerkras <Krasmax97@mail.ru>
2019-04-13 18:21:49 +0300

    fix test

--- test.js
+++ test.js
@@ -6,8 +6,9 @@ describe('Task API Routes', function() {
     // In this test it's expected a task list of two tasks
     describe('GET /', function() {
         it('returns a list of tasks', function(done) {
-            router.get('/tasks')
-                .expect(200);
+            router.get('/', function(req, res) {
+            res.expect(200);
+            });
             done()
         });
     });
